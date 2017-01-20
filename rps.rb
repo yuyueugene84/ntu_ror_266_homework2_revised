6937991387b8053ba21d3f15da498c7594da146c
yuyueugene84 <yuyueugene84@gmail.com>
2017-01-20 20:27:50 +0800

    removed unnecessary self

--- rps.rb
+++ rps.rb
@@ -1,8 +1,13 @@
 require_relative "player.rb"
 
 class RPS
-  # 剪刀石頭布這三種選項不會改變，應定義成 CONSTANT
-  HANDS = {R: "Rock", P: "Paper", S: "Scissors"}
+  # 剪刀石頭布這三種選項不會改變，應定義成 CONSTANT，之後在打印出結果時，可以很容易的把
+  # R/P/S 轉成 Rock / Paper / Scissors
+  HANDS = {
+            R: "Rock",
+            P: "Paper",
+            S: "Scissors"
+          }
 
   attr_accessor :human_player, :computer_player, :msg
 
@@ -17,9 +22,9 @@ class RPS
   def play
     intro  
     begin 
-      self.human_player.pick_hand
-      self.computer_player.pick_hand
-      self.msg = check_win(human_player.hand, computer_player.hand)
+      human_player.pick_hand
+      computer_player.pick_hand
+      msg = check_win(human_player.hand, computer_player.hand)
       print_result(self.human_player.hand, self.computer_player.hand, msg)
     end while continue? == "Y"
     goodbye
@@ -37,13 +42,14 @@ class RPS
         puts "Play Again?: Y / N"
         continue = gets.chomp.upcase
       end while !["Y", "N"].include?(continue)
-      continue
+      continue # 利用 method 會回傳最後一行程式碼執行的結果，
+               # 確保 continue 的值有被回傳
     end
 
     def intro
       # 我們在知道玩家的名字後就可把玩家的名字印出
       puts "|===============================================|" 
-      puts "   #{self.human_player.name}! Welcome to Rock Paper Scissors!!!"
+      puts "   #{human_player.name}! Welcome to Rock Paper Scissors!!!"
       puts "|===============================================|"
     end
 
@@ -57,7 +63,7 @@ class RPS
 
     # 把玩家、電腦的名字、和訊息印出
     def print_result(user_input, computer_input, message)
-      puts "#{self.human_player.name} choose #{HANDS[user_input.to_sym]}, computer chooses #{HANDS[computer_input.to_sym]}, #{self.human_player.name} #{message}!"
+      puts "#{human_player.name} choose #{HANDS[user_input.to_sym]}, computer chooses #{HANDS[computer_input.to_sym]}, #{human_player.name} #{message}!"
       puts "================================================="
     end
 
@@ -75,7 +81,7 @@ class RPS
         if computer_input == "R"
           "wins"
         elsif computer_input == "S"
-          return "loses"
+          "loses"
         end
       elsif user_input == "S"
         if computer_input == "P"
